64b13219c0fbfd272d9f8329c3fb37d127b5415b
JHelgesson <jonas.helgesson@softhouse.se>
2015-09-02 11:29:25 +0200

    Emptied existing callbacks on the d3nv dispatcher and added a custom event handler for 'elementClick' when the user clicks the chart to be able to parse the date the user selected.

--- js/main.js
+++ js/main.js
@@ -16,6 +16,65 @@
     d3.select('#chart svg').datum(data).call(chart);
     nv.utils.windowResize(chart.update);
 
+    function disableAreaClick() {
+      chart.stacked.dispatch.on("areaClick", null);
+      chart.stacked.dispatch.on("areaClick.toggle", null);
+  
+      chart.stacked.scatter.dispatch.on("elementClick", null);
+      
+      chart.interactiveLayer.dispatch.on("elementMousemove", null);
+      chart.stacked.scatter.dispatch.on("elementClick.area", null);
+  
+      chart.legend.dispatch.on("legendClick", null);
+      chart.legend.dispatch.on("legendDblclick", null);
+      chart.legend.dispatch.on("stateChange", null);
+  
+      if (chart.update) {  
+        var originalUpdate = chart.update; 
+        chart.update = function(){
+            originalUpdate();
+            disableAreaClick();
+        }
+      }
+    } 
+  
+    disableAreaClick();
+    
+    chart.interactiveLayer.dispatch.on("elementClick", function(e) {
+       console.log(e);
+       chart.clearHighlights();
+       
+       var that = this;
+       var singlePoint
+       var pointIndex;
+       var pointXLocation;
+       
+       data.filter(function(series, i) {
+            series.seriesIndex = i;
+            return !series.disabled;
+        }).forEach(function(series,i) {
+            pointIndex = nv.interactiveBisect(series.values, e.pointXValue, chart.x());
+            var point = series.values[pointIndex];
+            var pointYValue = chart.y()(point, pointIndex);
+            
+            if (pointYValue != null) {
+                chart.highlightPoint(i, pointIndex, true); // Highlight for each series..
+            }
+            
+            if (typeof point === 'undefined') 
+              return;
+              
+            if (typeof singlePoint === 'undefined') 
+              singlePoint = point;
+              
+            if (typeof pointXLocation === 'undefined') 
+              pointXLocation = chart.xScale()(chart.x()(point,pointIndex));
+        });
+
+        chart.interactiveLayer.renderGuideLine(pointXLocation);
+        console.log("Selected date: " + chart.xAxis.tickFormat()(chart.x()(singlePoint, pointIndex)));
+    });
+
     return chart;
   });
   
