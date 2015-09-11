ef2ebe81de19b6c8cdc5bbce787c250d2b39b61b
martingreer <greer@student.chalmers.se>
2015-09-11 16:23:49 +0100

    Added Base64 encoder+decoder for sending http requests. Did some code clean-up.

--- js/kanApp.js
+++ js/kanApp.js
@@ -1,4 +1,5 @@
 /*global angular, d3, get, console*/
+/*jslint bitwise: true, plusplus: true, white: true*/
 
 var kanApp = angular.module('kanApp', ['ui.router', 'nvd3']);
 
@@ -29,6 +30,93 @@ kanApp.config(function ($stateProvider, $urlRouterProvider) {
             url: '/about',
             templateUrl: 'pages/about.html'
         });
+});
+
+kanApp.factory('Base64', function () {
+    "use strict";
+    /*jslint regexp: true*/
+    
+    var keyStr = 'ABCDEFGHIJKLMNOP' +
+        'QRSTUVWXYZabcdef' +
+        'ghijklmnopqrstuv' +
+        'wxyz0123456789+/' +
+        '=';
+    return {
+        encode: function (input) {
+            var output = "",
+                chr1, chr2, chr3 = "",
+                enc1, enc2, enc3, enc4 = "",
+                i = 0;
+ 
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+ 
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+ 
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+                output = output +
+                    keyStr.charAt(enc1) +
+                    keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) +
+                    keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+ 
+            return output;
+        },
+ 
+        decode: function (input) {
+            var output = "",
+                chr1, chr2, chr3 = "",
+                enc1, enc2, enc3, enc4 = "",
+                i = 0,
+                base64test = /[^A-Za-z0-9\+\/\=]/g,
+                alert;
+ 
+            if (base64test.exec(input)) {
+                alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+ 
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+ 
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+ 
+                output = output + String.fromCharCode(chr1);
+ 
+                if (enc3 !== 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 !== 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+ 
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+ 
+            } while (i < input.length);
+            /*jslint regexp: false*/
+            return output;
+        }
+    };
 });
 
 kanApp.factory('dataService', function ($http) {
@@ -71,31 +159,6 @@ kanApp.controller('dataController', function ($scope, dataService) {
         } else {
             $scope.prettyString = null;
         }
-    };
-    
-    $scope.jsonPrettyPrint = function () {
-        var jsonData = $scope.data;
-        /*jslint regexp: true*/
-        if (typeof jsonData !== 'string') {
-            jsonData = JSON.stringify(jsonData, undefined, 2);
-        }
-        jsonData = jsonData.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-        $scope.prettyString = jsonData.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-            var cls = 'number';
-            if (/^"/.test(match)) {
-                if (/:$/.test(match)) {
-                    cls = 'key';
-                } else {
-                    cls = 'string';
-                }
-            } else if (/true|false/.test(match)) {
-                cls = 'boolean';
-            } else if (/null/.test(match)) {
-                cls = 'null';
-            }
-            /*jslint regexp: false*/
-            $scope.prettyString = '<span class="' + cls + '">' + match + '</span>';
-        });
     };
 });
 
