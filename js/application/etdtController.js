e974644723bba424ca385d08a075004b8ee5522b
martingreer <greer@student.chalmers.se>
2016-03-03 23:28:59 +0100

    Added reset button to ETDT graph filter

--- js/application/etdtController.js
+++ js/application/etdtController.js
@@ -63,37 +63,46 @@ application.controller('etdtController', function ($scope, localStorageHandler, 
      * Filters the issues to be shown in the graph up to the max Cycle Time value.
      */
     $scope.applyCycleTimeFilter = function (maxCycleTime) {
-        var allIssues = [],
-            filteredIssues = [];
+        var filteredIssues = [];
 
-        localStorageHandler.getIssues(function (issuesCallback) {
-            allIssues = issuesCallback.issues;
-
-            if(maxCycleTime === '' || maxCycleTime === null || maxCycleTime === ' ' || maxCycleTime === undefined){
-                try{
-                    $scope.data = createEtDtData("All issues", allIssues);
-                    Notification.success("Filter removed.");
-                    if(DEBUG){console.log("Filter removed.");}
-                } catch (error) {
-                    Notification.error("Removing filter failed.");
-                    $scope.data = [];
-                }
-            } else {
-                _.forEach(allIssues, function(issue){
-                    if(timeUtil.msToHours(issue.cycleTime) <= maxCycleTime && issue.cycleTime !== null){
-                        filteredIssues.push(issue);
-                    }
-                });
-
-                try{
-                    $scope.data = createEtDtData("All issues", filteredIssues);
-                    if(DEBUG){console.log("Graph data updated according to filter.");}
-                    Notification.success("Filter applied.");
-                } catch (error) {
-                    Notification.error("Filter failed.");
-                    $scope.data = [];
-                }
+        if(maxCycleTime === '' || maxCycleTime === null || maxCycleTime === ' ' || maxCycleTime === undefined){
+            try{
+                $scope.data = createEtDtData("All issues", issues);
+                Notification.success("Filter removed.");
+                if(DEBUG){console.log("Filter removed.");}
+            } catch (error) {
+                Notification.error("Removing filter failed.");
+                $scope.data = [];
             }
-        });
+        } else {
+            _.forEach(issues, function(issue){
+                if(timeUtil.msToHours(issue.cycleTime) <= maxCycleTime && issue.cycleTime !== null){
+                    filteredIssues.push(issue);
+                }
+            });
+
+            try{
+                $scope.data = createEtDtData("All issues", filteredIssues);
+                if(DEBUG){console.log("Graph data updated according to filter.");}
+                Notification.success("Filter applied.");
+            } catch (error) {
+                Notification.error("Filter failed.");
+                $scope.data = [];
+            }
+        }
+    };
+
+    /**
+     * Resets current graph filter.
+     */
+    $scope.resetCycleTimeFilter = function () {
+        try{
+            $scope.data = createEtDtData("All issues", issues);
+            Notification.success("Filter removed.");
+            if(DEBUG){console.log("Filter removed.");}
+        } catch (error) {
+            Notification.error("Removing filter failed.");
+            $scope.data = [];
+        }
     }
 });
